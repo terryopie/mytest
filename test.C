245dc91f41f4c4b57d9e3fa3f1d64d7388fd0207
Terry J. Opie <terry.opie@vertitron.com>
2013-04-01 16:26:40 -0500

    more crap.
    Extended description here.

--- test.C
+++ test.C
@@ -7,8 +7,11 @@ void main ( void )
     int cntr = nothing;
     while( 1 )
     {
+        output( "stuff here too" );
         cntr -= testing;
         if( cntr == 0 )
             cntr = nothing;
+
+        nop();
     }
 }
