fd5e16f03b92c064335008e6b5f9b2f8b8260819
Terry J. Opie <terry.opie@vertitron.com>
2013-04-02 09:27:30 -0500

    One

--- test.C
+++ test.C
@@ -18,6 +18,25 @@ void main ( void )
             break;
     }
 }
+
+
+
+void main ( void )
+{
+    int cntr = nothing;
+    while( 1 )
+    {
+        output( "stuff here too" );
+        cntr -= testing;
+        if( cntr == 0 )
+            cntr = nothing;
+
+        nop();
+
+        if( cntr == 1.5 )
+            break;
+    }
+}
 void main ( void )
 {
     int cntr = nothing;
