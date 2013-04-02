250c1aa870d952ca4ba52367742b01c9a326c111
Terry J. Opie <terry.opie@vertitron.com>
2013-04-02 13:38:45 -0500

    Fixes Issue #7

--- test.C
+++ test.C
@@ -19,6 +19,57 @@ void main ( void )
     }
 }
 
+void main ( void )
+{
+    int cntr = nothing;
+    while( 1 )
+    {
+        output( "stuff here too" );
+        cntr -= testing;
+        if( cntr == 0 )
+            cntr = nothing;
+
+        nop();
+
+        if( cntr == 1.5 )
+            break;
+    }
+}
+
+void main ( void )
+{
+    int cntr = nothing;
+    while( 1 )
+    {
+        output( "stuff here too" );
+        cntr -= testing;
+        if( cntr == 0 )
+            cntr = nothing;
+
+        nop();
+
+        if( cntr == 1.5 )
+            break;
+    }
+}
+
+void main ( void )
+{
+    int cntr = nothing;
+    while( 1 )
+    {
+        output( "stuff here too" );
+        cntr -= testing;
+        if( cntr == 0 )
+            cntr = nothing;
+
+        nop();
+
+        if( cntr == 1.5 )
+            break;
+    }
+}
+
 
 void main ( void )
 {
